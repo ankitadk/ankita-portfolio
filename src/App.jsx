22224d096e3debac703acd7a53b7edeef52bbf9c
Ankita_K <ankitakalangutkar17@gmail.com>
2024-10-30 11:46:06 +0530

    portfolio update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import User from "./assets/Photo.jpg";
+// import User from "./assets/Photo2.png";
 // import { FaTwitter } from "react-icons/fa";
 // import { FaLinkedin } from "react-icons/fa";
-import Exp1 from "./assets/splunk.jpg";
-import Exp2 from "./assets/buildSpace.jpg";
-import Exp3 from "./assets/m9.jpg";
+import Exp1 from "./assets/anantlogo.jpg";
+import Exp2 from "./assets/infiprelogo.png";
+import Exp3 from "./assets/sapnalogo.png";
+import Exp4 from "./assets/grassdoorlogo.jpg";
+import Exp5 from "./assets/coglogo.png";
 // import { ConfettiButton } from "@/components/magicui/confetti";
 import { ConfettiButton } from "@/components/ui/confetti";
 import BlurFade from "@/components/ui/blur-fade";
 import { ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 import AnimatedGradientText from "@/components/ui/animated-gradient-text";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -47,70 +51,131 @@ function App() {
               </a>
             </div> */}
             </div>
-            <img className="w-40 h-40 rounded-full" src={User} alt="" />
+            <img
+              className="w-40 h-40 rounded-full border border-slate-400"
+              src={User}
+              alt=""
+            />
           </div>
         </section>
 
         <section className="mt-12 max-w-3xl mx-auto">
           <h1 className="text-2xl font-bold">About Me</h1>
           <p className=" text-neutral-900">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
-            voluptatem corporis asperiores nemo in vitae magnam eos atque nam
-            doloremque,
+            Experienced Web Developer with over 7 years of expertise in building
+            dynamic web applications using PHP, React and Laravel. Skilled in
+            &nbsp;
             <span className="underline text-black">
-              accusantium a fugit eaque ipsam vero, dolorem quaerat accusamus
-              expedita!
+              full-stack development, REST APIs, Microservices, MySQL, and
+              integrating third-party services
             </span>
-            Totam quas ad quasi eveniet ut! Nihil molestias ipsum, commodi dolor
-            cum est earum repellat voluptates, obcaecati, laudantium hic.
+            . Successful at maintaining customer satisfaction through effective
+            customer support.
           </p>
         </section>
 
         <section className="mt-12 max-w-3xl mx-auto">
           <h1 className="text-2xl font-bold">Work Experience</h1>
           <p className=" text-neutral-900">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
-            voluptatem corporis asperi
+            Hands-on experience in full-stack development, REST API design, and
+            building scalable software solutions across diverse domains,
+            including pharma, e-learning, e-commerce, and retail.
           </p>
           <div className="mt-6">
             <div className="flex justify-between items-center">
               <div className="flex gap-3 items-center">
-                <img src={Exp1} className="h-12 rounded-full" alt="" />
+                <img
+                  src={Exp5}
+                  className="h-12 w-12 rounded-full object-contain"
+                  alt=""
+                />
                 <div className="">
-                  <h2 className="text-sm">Splunk Technologies</h2>
+                  <h2 className="text-sm">
+                    Cogncise Business Services Pvt. Ltd., Margao-Goa
+                  </h2>
+                  <p className="text-xs">Web Developer</p>
+                </div>
+              </div>
+              <p className="text-neutral-500 text-sm font-light">
+                Jan 2024 - Apr 2024
+              </p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div className="flex justify-between items-center">
+              <div className="flex gap-3 items-center">
+                <img
+                  src={Exp4}
+                  className="h-12 w-12 rounded-full object-contain"
+                  alt=""
+                />
+                <div className="">
+                  <h2 className="text-sm">
+                    Grassdoor Logistics Pvt. Ltd., Verna-Goa
+                  </h2>
+                  <p className="text-xs">Senior Software Engineer</p>
+                </div>
+              </div>
+              <p className="text-neutral-500 text-sm font-light">
+                Jan 2022 - Oct 2023
+              </p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div className="flex justify-between items-center">
+              <div className="flex gap-3 items-center">
+                <img
+                  src={Exp3}
+                  className="h-12 w-12 rounded-full object-contain"
+                  alt=""
+                />
+                <div className="">
+                  <h2 className="text-sm">sapnagroup, Panaji-Goa</h2>
+                  <p className="text-xs">Web Programmer</p>
+                </div>
+              </div>
+              <p className="text-neutral-500 text-sm font-light">
+                June 2021 - Dec 2022
+              </p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div className="flex justify-between items-center">
+              <div className="flex gap-3 items-center">
+                <img
+                  src={Exp2}
+                  className="h-12 w-12 rounded-full object-contain"
+                  alt=""
+                />
+                <div className="">
+                  <h2 className="text-sm">
+                    Infipre IT Services(OPC) Pvt. Ltd., Sanquelim-Goa
+                  </h2>
                   <p className="text-xs">Senior Software Developer</p>
                 </div>
               </div>
               <p className="text-neutral-500 text-sm font-light">
-                May 2021 - Dec 20201
+                Sept 2019 - May 2021
               </p>
             </div>
           </div>
           <div className="mt-6">
             <div className="flex justify-between items-center">
               <div className="flex gap-3 items-center">
-                <img src={Exp2} className="h-12 w-12 rounded-full" alt="" />
+                <img
+                  src={Exp1}
+                  className="h-12 w-12 rounded-full object-contain"
+                  alt=""
+                />
                 <div className="">
-                  <h2 className="text-sm">Build Space</h2>
+                  <h2 className="text-sm">
+                    Anant Infomedia Pvt. Ltd., Panaji-Goa
+                  </h2>
                   <p className="text-xs">Software Developer</p>
                 </div>
               </div>
               <p className="text-neutral-500 text-sm font-light">
-                May 2021 - Dec 20201
-              </p>
-            </div>
-          </div>
-          <div className="mt-6">
-            <div className="flex justify-between items-center">
-              <div className="flex gap-3 items-center">
-                <img src={Exp3} className="h-12 rounded-full" alt="" />
-                <div className="">
-                  <h2 className="text-sm">M Hacks 9</h2>
-                  <p className="text-xs">Frontend Developer</p>
-                </div>
-              </div>
-              <p className="text-neutral-500 text-sm font-light">
-                May 2021 - Dec 20201
+                Feb 2016 - Jan 2019
               </p>
             </div>
           </div>
@@ -119,14 +184,19 @@ function App() {
         <section className="mt-12 mb-36 max-w-3xl mx-auto">
           <h1 className="text-2xl font-bold">Get in Touch</h1>
           <p className=" text-neutral-900">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Accusantium cumque sapiente quam, laboriosam nemo necessitatibus
-            dolores.
+            I'm always excited to connect! Whether you have a project in mind, a
+            question, or just want to say hello, feel free to reach out. Let’s
+            create something great together!
           </p>
           <div className="relative">
-            <ConfettiButton className="mt-3 rounded-full px-5 font-light">
-              Text me on Twitter
-            </ConfettiButton>
+            <a
+              href="https://www.linkedin.com/in/ankita-kalangutkar-675a22184/"
+              target="_blank"
+            >
+              <ConfettiButton className="mt-3 rounded-full px-5 font-light">
+                Text me on Linkedin
+              </ConfettiButton>
+            </a>
           </div>
         </section>
       </BlurFade>
